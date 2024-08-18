432e5a7b747c8d7424d9cc22c35f04ebb6c01338
gtopinio <mctopinio@up.edu.ph>
2024-08-18 22:46:15 +0800

    release: initial commit

---
+++ src/main/java/github/gtopinio/STOMPaaS/StomPaaSApplication.java
@@ -0,0 +1,13 @@
+package github.gtopinio.STOMPaaS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StomPaaSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StomPaaSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/gtopinio/STOMPaaS/StomPaaSApplicationTests.java
@@ -0,0 +1,13 @@
+package github.gtopinio.STOMPaaS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StomPaaSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
